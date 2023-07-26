bdda3267776d83de7fdfeb7bfbd0789d28822233
João Pedro <jp663687@gmail.com>
2023-07-26 13:45:06 -0300

    adiciona `input`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  userName = 'João';
+  userData = {
+    email: 'jp663687@gmail.com',
+    telefone: 4499799784,
+    role: 'Admin',
+  };
   title = 'projeto-angular';
 }
