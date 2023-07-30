5c4ee3baac5024fba6454145ba734d543caee7de
João Pedro <jp663687@gmail.com>
2023-07-30 14:04:39 -0300

    adiciona ìnterfaces`

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,40 +1,42 @@
 import { Component } from '@angular/core';
 
+import { Animal } from 'src/app/Animal';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css'],
 })
 export class ListRenderComponent {
-  animals = [
+  animalDetails = '';
+  animals: Animal[] = [
     {
       name: 'Turca',
       type: 'Dog',
+      age: 4,
     },
     {
       name: 'Tom',
       type: 'Cat',
+      age: 10,
     },
     {
       name: 'Luna',
       type: 'Dog',
+      age: 15,
     },
     {
       name: 'Whiskers',
       type: 'Cat',
+      age: 2,
     },
     {
       name: 'Max',
       type: 'Dog',
-    },
-    {
-      name: 'Mittens',
-      type: 'Cat',
-    },
-    {
-      name: 'Charlie',
-      type: 'Dog',
+      age: 3,
     },
   ];
-  showMessege() {}
+  showAge(animal: Animal): void {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos`;
+  }
 }
