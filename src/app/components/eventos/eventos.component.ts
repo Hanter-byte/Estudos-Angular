b7f76fd2136878262298cbc427e3590287a88820
João Pedro <jp663687@gmail.com>
2023-07-30 13:39:42 -0300

    adciona `list-render`

--- src/app/components/eventos/eventos.component.ts
+++ src/app/components/eventos/eventos.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class EventosComponent {
   show: boolean = false;
+  classes = ['gren-title'];
 
   showMessege(): void {
     this.show = !this.show;
