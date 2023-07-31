c84978cd9f22f3caecd9d5c5a9eea6258244d9e6
João Pedro <jp663687@gmail.com>
2023-07-31 09:53:25 -0300

    adiciona `services`

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 
 import { Animal } from 'src/app/Animal';
 
+import { ListService } from 'src/app/services/list.service';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css'],
 })
 export class ListRenderComponent {
+  constructor(private listService: ListService) {}
+
   animalDetails = '';
   animals: Animal[] = [
     {
@@ -38,5 +42,9 @@ export class ListRenderComponent {
   ];
   showAge(animal: Animal): void {
     this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos`;
+  }
+  removeAnimal(animal: Animal) {
+    console.log('Removendo animal...');
+    this.animals = this.listService.remove(this.animals, animal);
   }
 }
